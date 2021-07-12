4d58293ba0b1c81728073556a52e3e08037a4490
leleliu008 <leleliu008@gmail.com>
2021-07-12 17:28:41 +0800

    optimize

--- formula/ffmpeg.sh
+++ formula/ffmpeg.sh
@@ -2,15 +2,15 @@ package set summary "Play, record, convert, and stream audio and video"
 package set webpage "https://ffmpeg.org"
 package set src.url "https://ffmpeg.org/releases/ffmpeg-4.2.3.tar.xz"
 package set src.sum "9df6c90aed1337634c1fb026fb01c154c29c82a64ea71291ff2da9aacb9aad31"
-package set bsystem "make"
 package set dep.pkg "sdl2 opus libwebp libvorbis libtheora speex x264 x265 lame"
+package set bsystem "make"
+package set binsrcd 'true'
+package set build_in_parallel false
 
 prepare() {
     sed_in_place 's/$_cc -v/$_cc --version/g' configure &&
     sed_in_place 's/cpuflags="-mcpu=$cpu"/[ -z "$cpu" ] || cpuflags="-mcpu=$cpu"/g' configure
 }
-
-package set binsrcd true
 
 build() {
     run ./configure \
