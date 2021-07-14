179ce588f545a8c1b8a1654d8cb8e7692929e211
leleliu008 <leleliu008@gmail.com>
2021-07-15 07:02:56 +0800

    optimize

--- formula/libogg.sh
+++ formula/libogg.sh
@@ -1,5 +1,6 @@
 package set summary "Ogg Bitstream Library"
 package set webpage "https://www.xiph.org/ogg"
-package set src.url "https://downloads.xiph.org/releases/ogg/libogg-1.3.4.tar.gz"
-package set src.sum "fe5670640bd49e828d64d2879c31cb4dde9758681bb664f9bdbf159a01b0c76e"
+package set src.git "https://github.com/xiph/ogg.git"
+package set src.url "https://github.com/xiph/ogg/releases/download/v1.3.4/libogg-1.3.4.tar.xz"
+package set src.sum "c163bc12bc300c401b6aa35907ac682671ea376f13ae0969a220f7ddf71893fe"
 package set bsystem "cmake"
