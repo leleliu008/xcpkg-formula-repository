e223ea9cad23b8cb9919cd8b611d749710716310
leleliu008 <leleliu008@gmail.com>
2021-08-15 00:05:07 +0800

    optimize

--- formula/gnutls.sh
+++ formula/gnutls.sh
@@ -2,8 +2,8 @@ package set summary "GNU Transport Layer Security (TLS) Library"
 package set webpage "https://gnutls.org"
 package set src.url "https://www.gnupg.org/ftp/gcrypt/gnutls/v3.6/gnutls-3.6.14.tar.xz"
 package set src.sum "5630751adec7025b8ef955af4d141d00d252a985769f51b4059e5affa3d39d63"
+package set dep.pkg "gmp libunistring nettle libtasn1 libidn2"
 package set bsystem "configure"
-package set dep.pkg "gmp libunistring nettle libtasn1 libidn2 p11-kit"
 
 # thread-local storage is not supported for the current target
 
@@ -14,9 +14,11 @@ prepare() {
 build() {
     configure \
         --without-gcov \
+        --without-p11-kit \
         --disable-valgrind-tests \
         --disable-code-coverage \
         --disable-gtk-doc \
         --disable-guile \
+        --disable-tests \
         --enable-gcc-warnings
 }
